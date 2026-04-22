c13cf7f3fa0eda5afe781fa20667ea6e9d1b6707
Nicolas Nascimento <devnicolasnasc@gmail.com>
2026-04-21 21:46:07 -0300

    Ajusta elementos do header e menu

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -18,13 +18,16 @@ import Link from "next/link"
 
 export function Headerr() {
   return (
-    <header className="bg-white fixed w-full">
-      <nav className="flex justify-between items-center p-1.5">
-        <Image src="/nico.svg" alt="Logo do site" width={90} height={50}/>
+    <header className="fixed w-full bg-white border-2 border-b-purple-600 shadow-black shadow-[0px_0px_10px_0px]">
+      <nav className="flex items-center justify-between p-1.5">
+        <Image src="/nico.svg" alt="Logo do site" width={70} height={50} />
         <div className="md:hidden">
           <DropdownMenu>
             <DropdownMenuTrigger asChild>
-              <Button variant="outline" className="w-10 h-10 bg-purple-950 hover:bg-purple-500">
+              <Button
+                variant="outline"
+                className="h-10 w-10 bg-purple-950 hover:bg-purple-500"
+              >
                 <div className="flex flex-col gap-0.5">
                   <div className="h-0.5 w-3 bg-white"></div>
                   <div className="h-0.5 w-3 bg-white"></div>
@@ -32,28 +35,36 @@ export function Headerr() {
                 </div>
               </Button>
             </DropdownMenuTrigger>
-            <DropdownMenuContent className="w-50 relative right-5 top-3 bg-purple-950 p-4" align="start">
+            <DropdownMenuContent
+              className="relative top-3 right-5 w-50 bg-purple-950 p-4"
+              align="start"
+            >
               <DropdownMenuGroup>
-                <DropdownMenuLabel className="text-gray-300 text-center">Menu principal</DropdownMenuLabel>
-                <DropdownMenuItem className="text-white font-[poppins] text-[13px]">Início</DropdownMenuItem>
-                <DropdownMenuItem className="text-white font-[poppins] text-[13px]">Saiba mais</DropdownMenuItem>
-                <DropdownMenuItem className="text-white  font-[poppins] text-[13px]">Contato</DropdownMenuItem>
+                <DropdownMenuItem className="text-[13px] text-white">
+                  Início
+                </DropdownMenuItem>
+                <DropdownMenuItem className="text-[13px] text-white">
+                  Saiba mais
+                </DropdownMenuItem>
+                <DropdownMenuItem className="text-[13px] text-white">
+                  Contato
+                </DropdownMenuItem>
               </DropdownMenuGroup>
             </DropdownMenuContent>
           </DropdownMenu>
         </div>
 
-        <div className="hidden md:block md:flex md:gap-5">
-          <Link href={""}>
-            <h1>Início</h1>
+        <div className="hidden md:block md:flex md:gap-5 md:pr-5">
+          <Link href={"/"}>
+            <h1 className="text-[14px]">Início</h1>
+          </Link>
+
+          <Link href={"/about"}>
+            <h1 className="text-[14px]">Saiba mais</h1>
           </Link>
 
           <Link href={""}>
-            <h1>Saiba mais</h1>
-          </Link>
-
-          <Link href={""}>
-            <h1>Contato</h1>
+            <h1 className="text-[14px]">Contato</h1>
           </Link>
         </div>
       </nav>
