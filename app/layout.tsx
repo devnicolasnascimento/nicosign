b0ff1a4373474f60f538010e98f4b39261ca96ca
Nicolas Nascimento <devnicolasnasc@gmail.com>
2026-04-28 00:44:53 -0300

    Adiciona metadados ao site

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,32 @@
 import { Geist, Geist_Mono, Instrument_Serif } from "next/font/google"
 
 import "./globals.css"
-import { cn } from "@/lib/utils";
-import { Headerr } from "@/components/header/header";
+import { cn } from "@/lib/utils"
+import { Headerr } from "@/components/header/header"
+import type { Metadata } from "next"
+import { url } from "node:inspector"
 
-const geist = Geist({subsets:['latin'],variable:'--font-sans'})
+export const metadata: Metadata = {
+  title: "Nicosign | Design criativo",
+  description:
+    "Design criativo voltado para identidade visual, branding e soluções estratégicas que fortalecem marcas e destacam negócios no digital.",
+  openGraph: {
+    title: "Nicosign | Design criativo",
+    description:
+      "Design criativo voltado para identidade visual, branding e soluções estratégicas que fortalecem marcas e destacam negócios no digital.",
+    url: "https://nicosign.vercel.app",
+    images: [
+      {
+        url: "/nico.svg",
+        width: 100,
+        height: 100,
+        alt: "Nicosign",
+      },
+    ],
+  },
+}
+
+const geist = Geist({ subsets: ["latin"], variable: "--font-sans" })
 
 const instrumentSerif = Instrument_Serif({
   subsets: ["latin"],
@@ -26,14 +48,17 @@ export default function RootLayout({
     <html
       lang="en"
       suppressHydrationWarning
-      className={cn("antialiased", fontMono.variable, "font-sans", geist.variable, instrumentSerif.variable)}
+      className={cn(
+        "antialiased",
+        fontMono.variable,
+        "font-sans",
+        geist.variable,
+        instrumentSerif.variable
+      )}
     >
-      <body className="bg-[#100029] overflow-x-hidden">
+      <body className="overflow-x-hidden bg-[#100029]">
         <Headerr />
-        <main className="pt-46">
-          {children}
-        </main>
-        
+        <main className="pt-46">{children}</main>
       </body>
     </html>
   )
