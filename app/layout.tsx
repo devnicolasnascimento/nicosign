75d3c746ce5374c53a3ca114c0131520140f5edf
Nicolas Nascimento <devnicolasnasc@gmail.com>
2026-04-28 00:48:34 -0300

    Altera caminho da imagem

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export const metadata: Metadata = {
     url: "https://nicosign.vercel.app",
     images: [
       {
-        url: "./public/nico.svg",
+        url: "nico.svg",
         width: 100,
         height: 100,
         alt: "Nicosign",
